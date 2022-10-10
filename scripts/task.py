2e19d151bbacac22cda72ea9d6d389d84fb42263
James Zhang <haozhizhang2021@u.northwestern.edu>
2022-10-09 22:56:27 -0500

    add task node and talker node

--- scripts/task.py
+++ scripts/task.py
@@ -190,13 +190,13 @@ class MoveGroupPythonInterfaceTutorial(object):
         ## We use the constant `tau = 2*pi <https://en.wikipedia.org/wiki/Turn_(angle)#Tau_proposals>`_ for convenience:
         # We get the joint values from the group and change some of the values:
         joint_goal = move_group.get_current_joint_values()
-        joint_goal[0] = 1.51
-        joint_goal[1] = -1.65
-        joint_goal[2] = -1.65
-        joint_goal[3] = -0.11
-        joint_goal[4] = 0.19
-        joint_goal[5] = 3.10 # 1/6 of a turn
-        joint_goal[6] = 0.84
+        joint_goal[0] = 0
+        joint_goal[1] = -tau / 8
+        joint_goal[2] = 0
+        joint_goal[3] = -tau / 4
+        joint_goal[4] = 0
+        joint_goal[5] = tau / 6  # 1/6 of a turn
+        joint_goal[6] = 0
 
         # The go command can be called with joint values, poses, or without any
         # parameters if you have already set the pose or joint target for the group
@@ -236,12 +236,12 @@ class MoveGroupPythonInterfaceTutorial(object):
 
         # The go command can be called with joint values, poses, or without any
         # parameters if you have already set the pose or joint target for the group
-        self.set_pub_caller(EmptyRequest())
+        self.set_pub_caller(EmptyRequest()) #call service to set topic publish flag to true
         move_group.go(joint_goal, wait=True)
 
         # Calling ``stop()`` ensures that there is no residual movement
         move_group.stop()
-        self.set_pub_caller(EmptyRequest())
+        self.set_pub_caller(EmptyRequest()) #call service to set topic publish flag to false
         
         ## END_SUB_TUTORIAL
 
@@ -263,16 +263,19 @@ class MoveGroupPythonInterfaceTutorial(object):
         ## end-effector:
         pose_goal = geometry_msgs.msg.Pose()
         pose_goal.orientation.w = 1.0
-        pose_goal.position.x = 0.4
-        pose_goal.position.y = 0.1
-        pose_goal.position.z = 0.4
+        pose_goal.position.x = 0.7
+        pose_goal.position.y = 0.0
+        pose_goal.position.z = 0.5
 
         move_group.set_pose_target(pose_goal)
 
+        self.set_pub_caller(EmptyRequest()) #call service to set topic publish flag to true
         ## Now, we call the planner to compute the plan and execute it.
         plan = move_group.go(wait=True)
         # Calling `stop()` ensures that there is no residual movement
         move_group.stop()
+        self.set_pub_caller(EmptyRequest()) #call service to set topic publish flag to false
+        
         # It is always good to clear your targets after planning with poses.
         # Note: there is no equivalent function for clear_joint_value_targets()
         move_group.clear_pose_targets()
@@ -413,104 +416,6 @@ class MoveGroupPythonInterfaceTutorial(object):
         return False
         ## END_SUB_TUTORIAL
 
-    def add_box(self, timeout=4):
-        # Copy class variables to local variables to make the web tutorials more clear.
-        # In practice, you should use the class variables directly unless you have a good
-        # reason not to.
-        box_name = self.box_name
-        scene = self.scene
-
-        ## BEGIN_SUB_TUTORIAL add_box
-        ##
-        ## Adding Objects to the Planning Scene
-        ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-        ## First, we will create a box in the planning scene between the fingers:
-        box_pose = geometry_msgs.msg.PoseStamped()
-        box_pose.header.frame_id = "panda_hand"
-        box_pose.pose.orientation.w = 1.0
-        box_pose.pose.position.z = 0.11  # above the panda_hand frame
-        box_name = "box"
-        scene.add_box(box_name, box_pose, size=(0.075, 0.075, 0.075))
-
-        ## END_SUB_TUTORIAL
-        # Copy local variables back to class variables. In practice, you should use the class
-        # variables directly unless you have a good reason not to.
-        self.box_name = box_name
-        return self.wait_for_state_update(box_is_known=True, timeout=timeout)
-
-    def attach_box(self, timeout=4):
-        # Copy class variables to local variables to make the web tutorials more clear.
-        # In practice, you should use the class variables directly unless you have a good
-        # reason not to.
-        box_name = self.box_name
-        robot = self.robot
-        scene = self.scene
-        eef_link = self.eef_link
-        group_names = self.group_names
-
-        ## BEGIN_SUB_TUTORIAL attach_object
-        ##
-        ## Attaching Objects to the Robot
-        ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-        ## Next, we will attach the box to the Panda wrist. Manipulating objects requires the
-        ## robot be able to touch them without the planning scene reporting the contact as a
-        ## collision. By adding link names to the ``touch_links`` array, we are telling the
-        ## planning scene to ignore collisions between those links and the box. For the Panda
-        ## robot, we set ``grasping_group = 'hand'``. If you are using a different robot,
-        ## you should change this value to the name of your end effector group name.
-        grasping_group = "hand"
-        touch_links = robot.get_link_names(group=grasping_group)
-        scene.attach_box(eef_link, box_name, touch_links=touch_links)
-        ## END_SUB_TUTORIAL
-
-        # We wait for the planning scene to update.
-        return self.wait_for_state_update(
-            box_is_attached=True, box_is_known=False, timeout=timeout
-        )
-
-    def detach_box(self, timeout=4):
-        # Copy class variables to local variables to make the web tutorials more clear.
-        # In practice, you should use the class variables directly unless you have a good
-        # reason not to.
-        box_name = self.box_name
-        scene = self.scene
-        eef_link = self.eef_link
-
-        ## BEGIN_SUB_TUTORIAL detach_object
-        ##
-        ## Detaching Objects from the Robot
-        ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-        ## We can also detach and remove the object from the planning scene:
-        scene.remove_attached_object(eef_link, name=box_name)
-        ## END_SUB_TUTORIAL
-
-        # We wait for the planning scene to update.
-        return self.wait_for_state_update(
-            box_is_known=True, box_is_attached=False, timeout=timeout
-        )
-
-    def remove_box(self, timeout=4):
-        # Copy class variables to local variables to make the web tutorials more clear.
-        # In practice, you should use the class variables directly unless you have a good
-        # reason not to.
-        box_name = self.box_name
-        scene = self.scene
-
-        ## BEGIN_SUB_TUTORIAL remove_object
-        ##
-        ## Removing Objects from the Planning Scene
-        ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-        ## We can remove the box from the world.
-        scene.remove_world_object(box_name)
-
-        ## **Note:** The object must be detached before we can remove it from the world
-        ## END_SUB_TUTORIAL
-
-        # We wait for the planning scene to update.
-        return self.wait_for_state_update(
-            box_is_attached=False, box_is_known=False, timeout=timeout
-        )
-
 
 def main():
     try:
@@ -521,22 +426,22 @@ def main():
         print("Press Ctrl-D to exit at any time")
         print("")
         input(
-            "============ Press `Enter` to begin the tutorial by setting up the moveit_commander ..."
+            "============ Press `Enter` to begin the task by setting up the moveit_commander ..."
         )
         tutorial = MoveGroupPythonInterfaceTutorial()
         
         input(
-            "============ Press `Enter` to Go Home"
+            "============ Press `Enter` to set Home position"
         )
         tutorial.go_to_home_state()
 
-        input(
-            "============ Press `Enter` to execute a movement using a joint state goal ..."
-        )
-        tutorial.go_to_joint_state()
+        # input(
+        #     "============ Press `Enter` to execute a movement using a joint state goal ..."
+        # )
+        # tutorial.go_to_joint_state()
 
-        # input("============ Press `Enter` to execute a movement using a pose goal ...")
-        # tutorial.go_to_pose_goal()
+        input("============ Press `Enter` to execute a movement using a pose goal ...")
+        tutorial.go_to_pose_goal()
 
         # input("============ Press `Enter` to plan and display a Cartesian path ...")
         # cartesian_plan, fraction = tutorial.plan_cartesian_path()
@@ -549,27 +454,7 @@ def main():
         # input("============ Press `Enter` to execute a saved path ...")
         # tutorial.execute_plan(cartesian_plan)
 
-        # input("============ Press `Enter` to add a box to the planning scene ...")
-        # tutorial.add_box()
-
-        # input("============ Press `Enter` to attach a Box to the Panda robot ...")
-        # tutorial.attach_box()
-
-        # input(
-        #     "============ Press `Enter` to plan and execute a path with an attached collision object ..."
-        # )
-        # cartesian_plan, fraction = tutorial.plan_cartesian_path(scale=-1)
-        # tutorial.execute_plan(cartesian_plan)
-
-        # input("============ Press `Enter` to detach the box from the Panda robot ...")
-        # tutorial.detach_box()
-
-        # input(
-        #     "============ Press `Enter` to remove the box from the planning scene ..."
-        # )
-        # tutorial.remove_box()
-
-        print("============ Python tutorial demo complete!")
+        print("============ Task complete!")
     except rospy.ROSInterruptException:
         return
     except KeyboardInterrupt:
